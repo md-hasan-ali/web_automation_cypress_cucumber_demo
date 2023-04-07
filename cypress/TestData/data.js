beb282c342c0c519289221ffcae77f99446be926
md-hasan-ali <hasanaliringku@gmail.com>
2023-04-08 00:57:32 +0600

    Web Automation with Cypress

---
+++ cypress/TestData/data.js
@@ -0,0 +1,16 @@
+class Data {
+
+    URL = "https://www.rokomari.com/book"
+    Email = "har.qups@gmail.com"
+    PASSWORD = "hasanhasan299519"
+    InvalidPassword = "asdfsdf"
+    NAME = "Hasan Ali"
+    PHONE = "01798-542100"
+    ADDRESS = "Thakurgaon"
+
+
+
+
+}
+
+export default Data;
